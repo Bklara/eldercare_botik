b4ad07c27901a8c6c5868b82f927f5706c92fd05
BKlara <brodskayaklara@gmail.com>
2024-10-01 13:40:27 +0300

    WIP

--- elder_care_bot/chatbot/views.py
+++ elder_care_bot/chatbot/views.py
@@ -1,12 +1,9 @@
-
-
-
 from rest_framework import viewsets  
 from rest_framework.decorators import api_view  
 from rest_framework.response import Response  
 from chatbot.models import Disease, BlogPost  
 from chatbot.serializers import DiseaseSerializer, BlogPostSerializer  
-from chatbot import process_user_input  
+from chatbot.chatbot_logic import process_user_input  
 
 class DiseaseViewSet(viewsets.ModelViewSet):  
     queryset = Disease.objects.all()  
